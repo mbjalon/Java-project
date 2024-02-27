e0dc6cd4dd7986881bee7b5ce20a3c86751cb4a3
Martin Bjaloň <martinbjal@gmail.com>
2024-02-27 19:06:15 +0100

    Project initalization

---
+++ ReservationSystemApp/src/main/java/impl/reservationsystemapp/ReservationSystemAppApplication.java
@@ -0,0 +1,13 @@
+package impl.reservationsystemapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservationSystemAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReservationSystemAppApplication.class, args);
+    }
+
+}
